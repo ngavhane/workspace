6f2fafa8342f69270c1e3fa3f7801fc3f0ddd043
Niteen Gavhane <niteen@calm.io>
2016-03-25 21:56:17 +0530

    implementation of socket

---
+++ pythonlearing/tcpServer.py
@@ -0,0 +1,21 @@
+import socket
+
+def Main():
+    host="127.0.0.1"
+    port=5006
+    s=socket.socket()
+    s.bind((host,port))
+    s.listen(1)
+    c,addres=s.accept()
+    print "Connection from :",str(addres)
+    while True:
+        data=c.recv(1024)
+        if not data:
+            break
+        print "Data received from connected side", str(data)
+        datatosent=data.upper()
+        print "Sending Capitalized data now"
+        c.send(str(data))
+    c.close()
+if __name__=="__main__":
+    Main()
