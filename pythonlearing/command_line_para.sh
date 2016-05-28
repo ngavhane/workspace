f74ea431acc96867f862455255ea6552c8d49421
Niteen Gavhane <niteen@calm.io>
2016-05-28 22:19:46 +0530

    shell script to study commnd line arguments

---
+++ pythonlearing/command_line_para.sh
@@ -0,0 +1,6 @@
+echo first argument is $0  # First parm is the name of the script
+echo second argument is $1  
+echo third argument is $2
+echo $*      #Will print all the parameter passed
+echo $?      #status code of the last executed 
+echo $@      #will print all the parameter passed
