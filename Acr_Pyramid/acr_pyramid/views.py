734429a72e12a45c4b39c3578fa3905bb88da752
Niteen Gavhane <niteen.gavhane@nutanix.com>
2017-07-30 19:04:50 +0530

    click on submit crash should open submit crash form

--- Acr_Pyramid/acr_pyramid/views.py
+++ Acr_Pyramid/acr_pyramid/views.py
@@ -1,10 +1,15 @@
 from pyramid.view import view_config
 from db_connect import connect
 
-@view_config(route_name='home', renderer='templates/mytemplate.jinja2')
-def my_view(request):
-    return {'project': 'Acr_Pyramid'}
+class Views(object):
+    
+    def __init__(self, request):
+        self.request = request
+        
+    @view_config(route_name='home', renderer='templates/mytemplate.jinja2')
+    def my_view(self):
+        return {'project': 'Acr_Pyramid'}
 
-@view_config(route_name='submit_crash', renderer='templates/submit_crash.jinja2')
-def my_view1(request):
-    return {'project': 'Acr_Pyramid'}
+    @view_config(route_name='submit_crash', renderer='templates/submit_crash.jinja2')
+    def my_view1(self):
+        return {'project': 'Acr_Pyramid'}
