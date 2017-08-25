26d3d63d7a0c3397385d466bf440082f197142dc
Niteen Gavhane <niteen.gavhane@nutanix.com>
2017-08-25 07:48:48 -0400

    binary seatch

--- python_modules/dsnalgo/binary_search.py
+++ python_modules/dsnalgo/binary_search.py
@@ -1,22 +1,25 @@
 
-def binary_search(list1, elememt):
-    if len(list1) ==1:
-        if elememt == list1[0]:
-            return True
+def binary_search(list1, find):
+    i=0
+    j=len(list1)-1
+    while(len(list1)!=1):
+        print list1
+        median = len(list1)/2
+        print median
+        if (list1[median]==find):
+           return True
+        elif(find > list1[median]):
+            list1=list1[median:]
         else:
-            return False
-    median = len(list1)/2
-    if elememt == list1[median]:
-        return True
-    elif(elememt > list1[median]):
-        binary_search(list1[median+1:], elememt)
-    else:
-        binary_search(list1[:median-1], elememt)
-        
-
+            list1=list1[:median]
+    return False
 if __name__ == "__main__":
-    ele = [1, 2, 3]
-    ele.sort()
-    print ele
-    elememt = raw_input("enter number that you want to search")
-    print binary_search(ele, int(elememt))
+    count = int(raw_input("Enter number of elements :"))
+    inputs = []
+    for i in range(count):
+        x=int(raw_input("enter %s the element:" % i))
+        inputs.append(x)
+    inputs.sort()
+    elememt = raw_input("enter number that you want to search:")
+    resp =  binary_search(list1=inputs, find=int(elememt))
+    print resp
